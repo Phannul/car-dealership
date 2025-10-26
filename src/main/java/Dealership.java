6a67af84afec5ae6b8641326212ec39864e2ee78
phannu <fanuelbirhane433@gmail.com>
2025-10-26 11:25:27 -0500

    created a vehicle and dealership class with empty methods but functional constructor

--- src/main/java/Dealership.java
+++ src/main/java/Dealership.java
@@ -1,4 +1,54 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class Dealership {
+    ArrayList<Vehicle> inventory;
+    private String name;
+    private String address;
+    private String phone;
+
+    public Dealership(String name, String address, String phone){
+        new ArrayList<>();
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+    // getters Section
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+    public void getVehiclesByPrice(double min, double max){
+
+    }
+    public void getVehiclesByMakeModel(String make, String model){
+
+    }
+    public void getVehiclesByYear(int min, int max){
+
+    }
+    public void  getVehiclesByColor(String color){
+
+    }
+    public void getVehiclesByMileage(int min, int max){
+
+    }
+    public void getVehicleByType(String type){
+
+    }
+    public void getAllVehicles(){
+
+    }
+    public void addVehicle(Vehicle vehicleToBeAdded){
+
+    }
+    public void removeVehicle(Vehicle vehicleToBeRemoved){
+
+    }
 }
--- src/main/java/Vehicle.java
+++ src/main/java/Vehicle.java
@@ -1,4 +1,47 @@
-package PACKAGE_NAME;
-
 public class Vehicle {
+    private int vin;
+    private int year;
+    private String make;
+    private String vehicleType;
+    private String color;
+    private int odometer;
+    private double price;
+
+    public Vehicle(int vin, int year, String make, String vehicleType, String color, int odometer, double price) {
+        this.vin = vin;
+        this.year = year;
+        this.make = make;
+        this.vehicleType = vehicleType;
+        this.color = color;
+        this.odometer = odometer;
+        this.price = price;
+    }
+
+    public int getVin() {
+        return vin;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getVehicleType() {
+        return vehicleType;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getOdometer() {
+        return odometer;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
